47c7c25d554284e5a5307b763cd05c027f66b845
Riley Croker <83546625+Riley-Croker@users.noreply.github.com>
2024-03-07 23:33:43 -0500

    Week 1 Day 2

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
   // PLAYER DATA
   const playersData = [
-    { name: "Dr. Mundo", team: "Dr Mundo’s Maulers", kda: "16.33", kills: 26, deaths: 3, assists: 23, cs: 312 },
-    { name: "Brand", team: "Dr Mundo’s Maulers", kda: "2.89", kills: 16, deaths: 9, assists: 10, cs: 285 },
-    { name: "Ashe", team: "Dr Mundo’s Maulers", kda: "3.67", kills: 14, deaths: 9, assists: 19, cs: 246 },
-    { name: "Soraka", team: "Dr Mundo’s Maulers", kda: "6.25", kills: 0, deaths: 4, assists: 25, cs: 152 },
-    { name: "Renekton", team: "Renekton’s Rampage", kda: "0.54", kills: 2, deaths: 13, assists: 5, cs: 136 },
-    { name: "Galio", team: "Renekton’s Rampage", kda: "1.2", kills: 7, deaths: 10, assists: 5, cs: 182 },
-    { name: "Miss Fortune", team: "Renekton’s Rampage", kda: "0.43", kills: 6, deaths: 14, assists: 0, cs: 180 },
-    { name: "Sivir", team: "Renekton’s Rampage", kda: "0.74", kills: 9, deaths: 19, assists: 5, cs: 249 },
-    { name: "Tristana", team: "Tristana’s Terror", kda: "1.43", kills: 1, deaths: 7, assists: 8, cs: 100 },
-    { name: "Garen", team: "Tristana’s Terror", kda: "1.6", kills: 6, deaths: 5, assists: 2, cs: 124 },
-    { name: "Lux", team: "Tristana’s Terror", kda: "1.33", kills: 4, deaths: 6, assists: 4, cs: 156 },
-    { name: "Caitlyn", team: "Tristana’s Terror", kda: "1.14", kills: 3, deaths: 7, assists: 5, cs: 152 },
-    { name: "Zilean", team: "Zilean’s Zodiac Warriors", kda: "1.71", kills: 4, deaths: 7, assists: 8, cs: 118 },
-    { name: "Cassiopeia", team: "Zilean’s Zodiac Warriors", kda: "14", kills: 7, deaths: 1, assists: 7, cs: 107 },
-    { name: "Malphite", team: "Zilean’s Zodiac Warriors", kda: "2", kills: 4, deaths: 4, assists: 4, cs: 91 },
-    { name: "Trundle", team: "Zilean’s Zodiac Warriors", kda: "7", kills: 10, deaths: 2, assists: 4, cs: 79 },
-    { name: "Warwick", team: "Warwick’s Warpath", kda: "0.5", kills: 1, deaths: 10, assists: 4, cs: 82 },
-    { name: "Annie", team: "Warwick’s Warpath", kda: "1.75", kills: 3, deaths: 4, assists: 4, cs: 98 },
-    { name: "Veigar", team: "Warwick’s Warpath", kda: "1", kills: 4, deaths: 9, assists: 5, cs: 101 },
-    { name: "Wukong", team: "Warwick’s Warpath", kda: "1.75", kills: 3, deaths: 4, assists: 4, cs: 125 },
-    { name: "Amumu", team: "Amumu’s Avengers", kda: "23", kills: 4, deaths: 1, assists: 19, cs: 82 },
-    { name: "Blitzcrank", team: "Amumu’s Avengers", kda: "18", kills: 1, deaths: 1, assists: 17, cs: 68 },
-    { name: "Jax", team: "Amumu’s Avengers", kda: "5.33", kills: 7, deaths: 3, assists: 9, cs: 82 },
-    { name: "Kayle", team: "Amumu’s Avengers", kda: "4.17", kills: 15, deaths: 6, assists: 10, cs: 107 },
-    { name: "Kog’maw", team: "Kog’maw’s Killers", kda: "2.43", kills: 6, deaths: 7, assists: 11, cs: 177 },
-    { name: "Karthus", team: "Kog’maw’s Killers", kda: "2.43", kills: 6, deaths: 7, assists: 11, cs: 162 },
-    { name: "Ziggs", team: "Kog’maw’s Killers", kda: "2.33", kills: 6, deaths: 6, assists: 8, cs: 260 },
-    { name: "Shen", team: "Kog’maw’s Killers", kda: "2.17", kills: 2, deaths: 6, assists: 11, cs: 98 },
-    { name: "Cho’gath", team: "Cho’gath Champions", kda: "1.83", kills: 7, deaths: 6, assists: 4, cs: 133 },
-    { name: "Ezreal", team: "Cho’gath Champions", kda: "3.67", kills: 16, deaths: 6, assists: 6, cs: 182 },
-    { name: "Leona", team: "Cho’gath Champions", kda: "2.4", kills: 3, deaths: 5, assists: 9, cs: 69 },
-    { name: "Master Yi", team: "Cho’gath Champions", kda: "1.1", kills: 5, deaths: 10, assists: 4, cs: 145 }
+    { name: "Dr. Mundo", team: "Dr Mundo’s Maulers", kda: "9.13", kills: 41, deaths: 8, assists: 32, cs: 477 },
+    { name: "Brand", team: "Dr Mundo’s Maulers", kda: "3.12", kills: 29, deaths: 17, assists: 24, cs: 443 },
+    { name: "Ashe", team: "Dr Mundo’s Maulers", kda: "2.32", kills: 19, deaths: 22, assists: 32, cs: 366 },
+    { name: "Soraka", team: "Dr Mundo’s Maulers", kda: "2.15", kills: 2, deaths: 20, assists: 41, cs: 193 },
+    { name: "Renekton", team: "Renekton’s Rampage", kda: "0.47", kills: 2, deaths: 15, assists: 5, cs: 230 },
+    { name: "Galio", team: "Renekton’s Rampage", kda: "0.93", kills: 8, deaths: 15, assists: 6, cs: 295 },
+    { name: "Miss Fortune", team: "Renekton’s Rampage", kda: "0.4", kills: 20, deaths: 14, assists: 0, cs: 266 },
+    { name: "Sivir", team: "Renekton’s Rampage", kda: "0.71", kills: 11, deaths: 24, assists: 6, cs: 438 },
+    { name: "Tristana", team: "Tristana’s Terror", kda: "1.5", kills: 5, deaths: 16, assists: 19, cs: 206 },
+    { name: "Garen", team: "Tristana’s Terror", kda: "2.92", kills: 20, deaths: 12, assists: 15, cs: 264 },
+    { name: "Lux", team: "Tristana’s Terror", kda: "3.21", kills: 17, deaths: 14, assists: 28, cs: 322 },
+    { name: "Caitlyn", team: "Tristana’s Terror", kda: "1.78", kills: 14, deaths: 18, assists: 18, cs: 289 },
+    { name: "Zilean", team: "Zilean’s Zodiac Warriors", kda: "2.6", kills: 6, deaths: 10, assists: 20, cs: 220 },
+    { name: "Cassiopeia", team: "Zilean’s Zodiac Warriors", kda: "8.33", kills: 17, deaths: 3, assists: 8, cs: 225 },
+    { name: "Malphite", team: "Zilean’s Zodiac Warriors", kda: "4.25", kills: 9, deaths: 4, assists: 8, cs: 263 },
+    { name: "Trundle", team: "Zilean’s Zodiac Warriors", kda: "10.5", kills: 11, deaths: 2, assists: 10, cs: 171 },
+    { name: "Warwick", team: "Warwick’s Warpath", kda: "1.41", kills: 9, deaths: 17, assists: 15, cs: 243 },
+    { name: "Annie", team: "Warwick’s Warpath", kda: "2.45", kills: 9, deaths: 11, assists: 18, cs: 210 },
+    { name: "Veigar", team: "Warwick’s Warpath", kda: "2.93", kills: 29, deaths: 14, assists: 12, cs: 399 },
+    { name: "Wukong", team: "Warwick’s Warpath", kda: "2.78", kills: 6, deaths: 9, assists: 19, cs: 218 },
+    { name: "Amumu", team: "Amumu’s Avengers", kda: "3.33", kills: 6, deaths: 9, assists: 24, cs: 176 },
+    { name: "Blitzcrank", team: "Amumu’s Avengers", kda: "7.8", kills: 13, deaths: 5, assists: 26, cs: 133 },
+    { name: "Jax", team: "Amumu’s Avengers", kda: "4.25", kills: 20, deaths: 8, assists: 14, cs: 244 },
+    { name: "Kayle", team: "Amumu’s Avengers", kda: "2.85", kills: 18, deaths: 13, assists: 19, cs: 266 },
+    { name: "Kog’maw", team: "Kog’maw’s Killers", kda: "2.08", kills: 10, deaths: 13, assists: 17, cs: 307 },
+    { name: "Karthus", team: "Kog’maw’s Killers", kda: "1.72", kills: 14, deaths: 18, assists: 17, cs: 363 },
+    { name: "Ziggs", team: "Kog’maw’s Killers", kda: "1.63", kills: 16, deaths: 16, assists: 10, cs: 501 },
+    { name: "Shen", team: "Kog’maw’s Killers", kda: "2.27", kills: 4, deaths: 11, assists: 21, cs: 203 },
+    { name: "Cho’gath", team: "Cho’gath Champions", kda: "1.83", kills: 13, deaths: 12, assists: 9, cs: 268 },
+    { name: "Ezreal", team: "Cho’gath Champions", kda: "2.22", kills: 25, deaths: 18, assists: 15, cs: 363 },
+    { name: "Leona", team: "Cho’gath Champions", kda: "2.58", kills: 9, deaths: 12, assists: 22, cs: 152 },
+    { name: "Master Yi", team: "Cho’gath Champions", kda: "0.81", kills: 8, deaths: 27, assists: 14, cs: 241 }
   ];
 
   // GAME DATA
@@ -53,19 +53,23 @@
     "March 6th G1": { winner: "Dr Mundo’s Maulers", loser: "Renekton’s Rampage" },
     "March 6th G2": { winner: "Zilean’s Zodiac Warriors", loser: "Tristana’s Terror" },
     "March 6th G3": { winner: "Amumu’s Avengers", loser: "Warwick’s Warpath" },
-    "March 6th G4": { winner: "Cho’gath Champions", loser: "Kog’maw’s Killers" }
+    "March 6th G4": { winner: "Cho’gath Champions", loser: "Kog’maw’s Killers" },
+    "March 7th G1": { winner: "Amumu’s Avengers", loser: "Kog’maw’s Killers" },
+    "March 7th G2": { winner: "Warwick’s Warpath", loser: "Cho’gath Champions" },
+    "March 7th G3": { winner: "Zilean’s Zodiac Warriors", loser: "Renekton’s Rampage" },
+    "March 7th G4": { winner: "Dr Mundo’s Maulers", loser: "Tristana’s Terror" }
   }
 
   // TEAM DATA
   var teamData = {
-    "Amumu’s Avengers": { wins: 0, losses: 0, captain: "Amumu", player1: "Blitzcrank", player2: "Jax", player3: "Kayle", nextMatch: "Kog’maw’s Killers", nextMatchDate: "March 7th" },
-    "Cho’gath Champions": { wins: 0, losses: 0, captain: "Cho’gath", player1: "Ezreal", player2: "Leona", player3: "Master Yi", nextMatch: "Warwick’s Warpath", nextMatchDate: "March 7th" },
-    "Dr Mundo’s Maulers": { wins: 0, losses: 0, captain: "Dr. Mundo", player1: "Brand", player2: "Soraka", player3: "Ashe", nextMatch: "Tristana’s Terror", nextMatchDate: "March 7th" },
-    "Kog’maw’s Killers": { wins: 0, losses: 0, captain: "Kog’maw", player1: "Karthus", player2: "Ziggs", player3: "Shen", nextMatch: "Amumu’s Avengers", nextMatchDate: "March 7th" },
-    "Renekton’s Rampage": { wins: 0, losses: 0, captain: "Renekton", player1: "Galio", player2: "Miss Fortune", player3: "Sivir", nextMatch: "Zilean’s Zodiac Warriors", nextMatchDate: "March 7th" },
-    "Tristana’s Terror": { wins: 0, losses: 0, captain: "Tristana", player1: "Garen", player2: "Lux", player3: "Caitlyn", nextMatch: "Dr Mundo’s Maulers", nextMatchDate: "March 7th" },
-    "Warwick’s Warpath": { wins: 0, losses: 0, captain: "Warwick", player1: "Annie", player2: "Veigar", player3: "Wukong", nextMatch: "Cho’gath Champions", nextMatchDate: "March 7th" },
-    "Zilean’s Zodiac Warriors": { wins: 0, losses: 0, captain: "Zilean", player1: "Cassiopeia", player2: "Malphite", player3: "Trundle", nextMatch: "Renekton’s Rampage", nextMatchDate: "March 7th" }
+    "Amumu’s Avengers": { wins: 0, losses: 0, captain: "Amumu", player1: "Blitzcrank", player2: "Jax", player3: "Kayle", nextMatch: "Renekton’s Rampage", nextMatchDate: "March 13th" },
+    "Cho’gath Champions": { wins: 0, losses: 0, captain: "Cho’gath", player1: "Ezreal", player2: "Leona", player3: "Master Yi", nextMatch: "Tristana’s Terror", nextMatchDate: "March 13th" },
+    "Dr Mundo’s Maulers": { wins: 0, losses: 0, captain: "Dr. Mundo", player1: "Brand", player2: "Soraka", player3: "Ashe", nextMatch: "Warwick’s Warpath", nextMatchDate: "March 13th" },
+    "Kog’maw’s Killers": { wins: 0, losses: 0, captain: "Kog’maw", player1: "Karthus", player2: "Ziggs", player3: "Shen", nextMatch: "Zilean’s Zodiac Warriors", nextMatchDate: "March 13th" },
+    "Renekton’s Rampage": { wins: 0, losses: 0, captain: "Renekton", player1: "Galio", player2: "Miss Fortune", player3: "Sivir", nextMatch: "Amumu’s Avengers", nextMatchDate: "March 13th" },
+    "Tristana’s Terror": { wins: 0, losses: 0, captain: "Tristana", player1: "Garen", player2: "Lux", player3: "Caitlyn", nextMatch: "Cho’gath Champions", nextMatchDate: "March 13th" },
+    "Warwick’s Warpath": { wins: 0, losses: 0, captain: "Warwick", player1: "Annie", player2: "Veigar", player3: "Wukong", nextMatch: "Dr Mundo’s Maulers", nextMatchDate: "March 13th" },
+    "Zilean’s Zodiac Warriors": { wins: 0, losses: 0, captain: "Zilean", player1: "Cassiopeia", player2: "Malphite", player3: "Trundle", nextMatch: "Kog’maw’s Killers", nextMatchDate: "March 13th" }
   };
 
   function calculateTeamData() {
